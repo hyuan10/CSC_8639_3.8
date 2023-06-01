2b2e3bb16d0d19cda9543edbcf6cc47db1d7ca2d
hyuan10 <94246352+hyuan10@users.noreply.github.com>
2023-06-01 18:14:12 +0100

    Adding Export to CSV Function

--- tobii_research_SDK.py
+++ tobii_research_SDK.py
@@ -1,4 +1,5 @@
 import tobii_research as tr
+import csv
 import time
 
 ## Finds the Eye tracker
@@ -13,10 +14,29 @@ print("Serial number: " + my_eyetracker.serial_number)
 
 ## Callback function for data
 def gaze_data_callback(gaze_data):
-    # Print gaze points of left and right eye
+    ## Print gaze points of left and right eye
     print("Left eye: ({gaze_left_eye}) \t Right eye: ({gaze_right_eye})".format(
         gaze_left_eye=gaze_data['left_gaze_point_on_display_area'],
         gaze_right_eye=gaze_data['right_gaze_point_on_display_area']))
+
+    ## Save gaze data to CSV
+    save_gaze_data_to_csv(gaze_data)
+
+
+## Save data function
+def save_gaze_data_to_csv(gaze_data):
+    timestamp = time.strftime("%Y%m%d_%H%M%S")  # Generate timestamp
+    filename = f"gaze_data_{timestamp}.csv"
+
+    ## Extract gaze points
+    gaze_left_eye = gaze_data['left_gaze_point_on_display_area']
+    gaze_right_eye = gaze_data['right_gaze_point_on_display_area']
+
+    ## Save gaze data to CSV
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Left Eye X', 'Left Eye Y', 'Right Eye X', 'Right Eye Y'])
+        writer.writerow([gaze_left_eye[0], gaze_left_eye[1], gaze_right_eye[0], gaze_right_eye[1]])
 
 ## Subscribe to data
 my_eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, gaze_data_callback, as_dictionary=True)
@@ -24,6 +44,5 @@ my_eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, gaze_data_callback, as_dicti
 ## Time interval
 time.sleep(5)
 
-## Unsubscrube from data
+## Unsubscribe from data
 my_eyetracker.unsubscribe_from(tr.EYETRACKER_GAZE_DATA, gaze_data_callback)
-
