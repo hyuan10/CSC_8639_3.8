91ceac880bcbd789beec947539f42c43c1220db6
hyuan10 <94246352+hyuan10@users.noreply.github.com>
2023-08-01 03:58:44 +0100

    Removing NaN and cleaning Data

--- Plot Data/src/1.R
+++ Plot Data/src/1.R
@@ -5,7 +5,14 @@ library(readr)
 
 
 # Load the gaze data
-data <- read_csv("data/gaze_data_000_20230730_222250.csv")
+data <- read_csv("data/gaze_data.csv", col_names = FALSE)
+
+col_names <- c("Left Eye X", "Left Eye Y", "Right Eye X", "Right Eye Y")
+
+colnames(data) <- col_names
+
+# Removing NaN from the data
+data <- data[complete.cases(data), ]
 
 # Calculate the average gaze point between left and right eyes
 data$Average_X <- (data$`Left Eye X` + data$`Right Eye X`) / 2
@@ -13,7 +20,7 @@ data$Average_Y <- (data$`Left Eye Y` + data$`Right Eye Y`) / 2
 
 # Create the heatmap for the average gaze points with smaller circles (h = 0.02) and without labels, grid, and legend
 average_gaze_heatmap <- ggplot(data, aes(x = Average_X, y = Average_Y)) +
-  stat_density_2d(aes(fill = ..level..), geom = "polygon", h = 0.05) +
+  stat_density_2d(aes(fill = ..level..), geom = "polygon", h = 0.08) +
   scale_fill_gradientn(colors = RColorBrewer::brewer.pal(9, "YlOrRd")) +
   theme_void() +
   theme(legend.position = "none")
