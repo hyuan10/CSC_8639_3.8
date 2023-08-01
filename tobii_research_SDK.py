c26d812e41e0830011b985fb7ad50189e8bee9be
hyuan10 <94246352+hyuan10@users.noreply.github.com>
2023-08-01 02:45:25 +0100

    File Path Update

--- tobii_research_SDK.py
+++ tobii_research_SDK.py
@@ -9,7 +9,7 @@ def run_eye_tracking():
     my_eyetracker = found_eyetrackers[0]
 
     # Create CSV file and writer for main CSV
-    folder_path = "/Users/harvey/IdeaProjects/CSC_8639_3.8/Gaze Data"
+    folder_path = "/Users/harvey/IdeaProjects/CSC_8639_3.8/Plot Data/data/Gaze Data"
     main_filename = f"{folder_path}/gaze_data.csv"
     main_csv_file = open(main_filename, mode='a', newline='')
     main_writer = csv.writer(main_csv_file)
